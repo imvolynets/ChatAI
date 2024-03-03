0331c9bb1e02bcdf458e8a555bae5554a1a410b3
Vladislav <146387960+volynetsdev@users.noreply.github.com>
2024-03-03 17:27:15 +0200

    add chat alert designed

--- ChatAI/Modules/ChatsListScreen/Controller/ChatsListViewController.swift
+++ ChatAI/Modules/ChatsListScreen/Controller/ChatsListViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChatsListViewController: UIViewController {
     private let mainView = ChatsListView()
     private let chats = Chat.example
+    private var addChatAlert: UIAlertController?
     
     override func loadView() {
         view = mainView
@@ -34,6 +35,7 @@ class ChatsListViewController: UIViewController {
     private func initViewController() {
         setupSearchField()
         setupTableView()
+        setupButtons()
     }
 }
 
@@ -47,12 +49,96 @@ extension ChatsListViewController {
     private func setupSearchField() {
         mainView.searchField.delegate = self
     }
+    
+    private func setupButtons() {
+        mainView.addNewChatButton.addTarget(self, action: #selector(didTapAddNewChat), for: .touchUpInside)
+    }
+}
+
+extension ChatsListViewController {
+    @objc
+    private func didTapAddNewChat() {
+        if addChatAlert == nil {
+            addChatAlert = createAddChatAlert()
+            setupAddChatAlert(addChatAlert: addChatAlert)
+        }
+        
+        if let addChatAlert {
+            presentAddChatAlert(addChatAlert: addChatAlert)
+        }
+    }
 }
 
 extension ChatsListViewController {
     private func openChat() {
         let chatViewController = ChatViewController()
         navigationController?.pushViewController(chatViewController, animated: true)
+    }
+}
+
+extension ChatsListViewController {
+    private func createAddChatAlert() -> UIAlertController {
+        return UIAlertController(
+            title: String(localized: "create_chat_alert_title"),
+            message: String(localized: "create_chat_alert_desc"),
+            preferredStyle: .alert
+        )
+    }
+    
+    private func setupAddChatAlert(addChatAlert: UIAlertController?) {
+        guard let addChatAlert else {
+            return
+        }
+
+        addChatAlert.addTextField { textField in
+            textField.delegate = self
+            textField.addTarget(
+                self,
+                action: #selector(self.textFieldDidChange(_:)),
+                for: .editingChanged
+            )
+        }
+
+        addChatAlert.addAction(UIAlertAction(
+            title: String(localized: "create_chat_alert_create_btn"),
+            style: .default
+        ) { _ in
+            guard let textField = addChatAlert.textFields?.first else {
+                return
+            }
+            
+            if let newChatName = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines) {
+                textField.text = ""
+                self.openChat()
+            }
+        })
+        
+        addChatAlert.addAction(UIAlertAction(
+            title: String(localized: "create_chat_alert_cancel_btn"),
+            style: .cancel,
+            handler: nil)
+        )
+    }
+    
+    private func presentAddChatAlert(addChatAlert: UIAlertController) {
+        let textCount = addChatAlert.textFields?.first?.text?.count ?? 0
+        addChatAlert.actions.first?.isEnabled = textCount <= 15 && textCount > 0
+        present(addChatAlert, animated: true, completion: nil)
+    }
+    
+    @objc
+    func textFieldDidChange(_ textField: UITextField) {
+        let characterLimit = 15
+        
+        if let text = textField.text, text.count > characterLimit {
+            let truncatedText = String(text.prefix(characterLimit))
+            textField.text = truncatedText
+        }
+        
+        if let alertController = presentedViewController as? UIAlertController {
+            let textCount = textField.text?.count ?? 0
+            alertController.actions.first?.isEnabled = textCount <= 15 && textCount > 0
+        }
     }
 }
 
@@ -85,6 +171,14 @@ extension ChatsListViewController: UITextFieldDelegate {
         if let text = textField.text {
             mainView.searchPlaceholderLabel.isHidden = !text.isEmpty
         }
+    }
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        if range.location == 0 && string == " " {
+            return false
+        }
+        return true
     }
 }
 
